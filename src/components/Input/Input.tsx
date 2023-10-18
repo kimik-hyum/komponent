0f919c9d9623595549825c9ac78a083be6ac6a13
kimik-hyum <familykim01@naver.com>
2023-10-19 00:01:02 +0900

    input type 제한 및 props 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,35 +1,82 @@
 import * as React from "react";
 import style from "./Input.module.scss";
-import { ComponentRef, forwardRef } from "react";
-import { useFocus } from "src/hook/useEvent";
 import { CommonComponentProps, SizeType } from "src/type/commonTypes";
+
+export enum InputType {
+  TEXT = "text",
+  PASSWORD = "password",
+  EMAIL = "email",
+  SEARCH = "search",
+  URL = "url",
+  TEL = "tel",
+  NUMBER = "number",
+}
 
 export interface InputProps extends CommonComponentProps<"input", "size"> {
   label: string;
+  startAdornment?: React.ReactNode;
+  endAdornment?: React.ReactNode;
+  type?: InputType;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ label, id, size, ...props }, ref): JSX.Element => {
+  (
+    {
+      label,
+      id,
+      size,
+      onFocus: userOnFocus,
+      onBlur: userOnBlur,
+      startAdornment,
+      endAdornment,
+      type = InputType.TEXT,
+      ...props
+    },
+    ref
+  ): JSX.Element => {
     const [focused, setFocused] = React.useState(false);
+    if (!Object.values(InputType).includes(type)) {
+      throw new Error(`Invalid input type: "${type}".`);
+    }
+
     return (
-      <div className={style.inputWrap}>
+      <div className={`${style.inputWrap} ${style["kui-input-root"]}`}>
+        {startAdornment && (
+          <span className={style["kui-input-startAdornment"]}>
+            {startAdornment}
+          </span>
+        )}
         <input
           type="text"
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
+          onFocus={(e) => {
+            setFocused(true);
+            userOnFocus?.(e);
+          }}
+          onBlur={(e) => {
+            setFocused(false);
+            userOnBlur?.(e);
+          }}
           {...props}
           {...{ ref, id, focused }}
-          className={style.input}
+          className={`${style.input} ${style["kui-input-base"]}`}
         />
-        <label htmlFor={id} className={style.label}>
+        {endAdornment && (
+          <span className={style["kui-input-endAdornment"]}>
+            {endAdornment}
+          </span>
+        )}
+        <label
+          htmlFor={id}
+          className={`${style.label} ${style["kui-input-label"]}`}
+        >
           {label}
         </label>
-        <fieldset className={style.fieldset}>
-          {
-            <legend>
-              <span>{label}</span>
-            </legend>
-          }
+        <fieldset
+          className={`${style.fieldset} ${style["kui-input-fieldset"]}`}
+        >
+          <legend className={style["kui-input-legend"]}>
+            <span>{label}</span>
+          </legend>
         </fieldset>
       </div>
     );
