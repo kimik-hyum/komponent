2028ebcd38a9a3f1dda8c99b14bc2f558b126948
khyunjun <familykim01@naver.com>
2023-12-04 17:16:09 +0900

    아코디언 수정

--- src/components/Accordion/Item.tsx
+++ src/components/Accordion/Item.tsx
@@ -1,6 +1,5 @@
 import React, { ReactNode, useContext } from "react";
-
-type CombinedType = AccordionContextType & AccordionItemProps;
+import { AccordionContext } from "./Root";
 
 type AccordionContextType = {
   disabled?: boolean;
@@ -8,21 +7,30 @@ type AccordionContextType = {
 };
 
 type AccordionItemProps = {
-  children: ReactNode;
+  children: ((context: { active: boolean }) => ReactNode) | ReactNode;
   className?: string;
 };
 
 export const ItemContext = React.createContext<AccordionContextType>({});
 
-export const AccordionItem: React.FC<CombinedType> = ({
+export const AccordionItem: React.FC<AccordionItemProps & AccordionContextType> = ({
   value,
   disabled,
   children,
   ...props
 }) => {
+  const { activeItem } = useContext(AccordionContext) || {};
+  const isActive = activeItem === value;
+
+  const isFunction = (child: any): child is (context: { active: boolean }) => ReactNode => {
+    return typeof child === 'function';
+  }
+
   return (
     <ItemContext.Provider value={{ value, disabled }}>
-      <div>{children}</div>
+      <div>
+        {isFunction(children) ? children({ active: isActive }) : children}
+      </div>
     </ItemContext.Provider>
   );
 };
--- stories/Accordion.stories.tsx
+++ stories/Accordion.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Accordion from "../src/components/Accordion";
+import * as Accordion from "../src/components/Accordion/index";
 
 export default {
   title: "Example/Accordion",
@@ -10,33 +10,26 @@ export default {
   tags: ["autodocs"],
 };
 
-// export const SimpleAccordion = () => (
-//   <Accordion.Root>
-//     <Accordion.Item value="1">
-//       <Accordion.Trigger>Item 1</Accordion.Trigger>
-//       <Accordion.Content>Content 1</Accordion.Content>
-//     </Accordion.Item>
+export const SimpleAccordion = () => (
+  <Accordion.Root>
+    <Accordion.Item value="1">
+      {({ active }) => {
+        return (
+          <>
+          <Accordion.Trigger>Item 1</Accordion.Trigger>
+          <Accordion.Content>Content 1</Accordion.Content>
+        </>
+      )}}
+    </Accordion.Item>
 
-//     <Accordion.Item value="2">
-//       <Accordion.Trigger>Item 2</Accordion.Trigger>
-//       <Accordion.Content>Content 2</Accordion.Content>
-//     </Accordion.Item>
+    <Accordion.Item value="2">
+      <Accordion.Trigger>Item 2</Accordion.Trigger>
+      <Accordion.Content>Content 2</Accordion.Content>
+    </Accordion.Item>
 
-//     <Accordion.Item value="3">
-//       <Accordion.Trigger>Item 3</Accordion.Trigger>
-//       <Accordion.Content>Content 3</Accordion.Content>
-//     </Accordion.Item>
-//   </Accordion.Root>
-// );
-
-const data = [
-  { title: "Watercraft", content: "Sample accordion content", value: "1" },
-  { title: "Automobiles", content: "Sample accordion content", value: "2" },
-  { title: "Aircrafts", content: "Sample accordion content", value: "3" },
-]
-
-export const HeadlessAccordion = () => (
-
-  <Accordion data={data} />
-
-)
+    <Accordion.Item value="3">
+      <Accordion.Trigger>Item 3</Accordion.Trigger>
+      <Accordion.Content>Content 3</Accordion.Content>
+    </Accordion.Item>
+  </Accordion.Root>
+);
